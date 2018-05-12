1cf60ba4fd63e634a731b96942206adef64565b7
matiplo <mateuszplodczyk@gmail.com>
2018-05-12 17:32:41 +0200

    ExtraActivity added

---
+++ app/src/main/java/com/example/ppp/myapplication/ExtraActivity.java
@@ -0,0 +1,13 @@
+package com.example.ppp.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ExtraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra);
+    }
+}
